e92bb248f1153cf8ea1585a1004c65a6b0290bec
Michael Odermatt <43145047+MichaelOdermatt@users.noreply.github.com>
2023-07-13 20:51:05 -0600

    Cleaning
    - Updated all functions to regular js functions.
    
    - Removed unnecessary getters and setters
    
    - Removed unused variables.

--- javascript/homescript.js
+++ javascript/homescript.js
@@ -20,9 +20,6 @@ const selectors = Object.freeze({
     touchscreenBtn: '#touchscreen_button',
 });
  
-//get cord end div
-var cord_end_div = document.querySelector(selectors.cord.endDiv);
-
 let is_plugged_in = false;
 let is_holding = false;
 let is_hovering = false;
@@ -32,7 +29,6 @@ let plug_offset_x = 55;
 let plug_offset_y = 25;
 let cord_offset_min = 100;
 let cord_offset_max = 200;
-let cord_shadow_offset = 50;
 let cord_svg_ceil = getElementOffset(document.querySelector(selectors.monitor.parent)).top;
 let play_area_floor = 0;
 let play_area_ceil = 0;
@@ -52,8 +48,56 @@ let small_prong_female_x_offset = 9;
 let has_sound_click_played = false;
 let sound_click = new Audio("sounds/click.mp3");
 
+// update the boundries of where the plug can exist
+update_screen_dependent_variables();
+move_plug(get_play_area_center_x(), play_area_ceil + get_play_area_height_half());
+
+// when the mouse cursor is over the plug
+plug_element.addEventListener('mouseover', () => {
+    is_hovering = true;
+});
+
+// when the mosue cursor is no longer over the plug
+plug_element.addEventListener('mouseout', () => {
+    is_hovering = false;
+});
+
+// when the mouse moves
+window.addEventListener('mousemove', (event) => {
+    if (is_holding) {
+        move_plug(event.pageX, event.pageY);
+    }
+});
+
+// when the left mouse button is pressed
+window.addEventListener('mousedown', () => {
+    if (is_hovering) {
+        is_holding = true;
+    }
+});
+
+// when the left mouse button is let go
+window.addEventListener('mouseup', () => {
+    if (is_holding) {
+        is_holding = false;
+    }
+});
+
+// button for users on mobile devices (the plug cant be moved on mobile/touchscreen devices)
+document.querySelector(selectors.touchscreenBtn).addEventListener('click', (e) => {
+    e.preventDefault();
+    move_plug_to_socket();
+    return false;
+});
+
+// on window resize, update the plug position and the bounding box where the plug can exist
+window.addEventListener('resize', () => {
+    update_screen_dependent_variables();
+    move_plug(plug_x, plug_y);
+});
+
 // function to encapsulate all plug movement logic
-move_plug = function (new_x, new_y) {
+function move_plug(new_x, new_y) {
     update_plug_position(new_x, new_y)
     check_plug_collision();
     update_plug_element_position();
@@ -61,19 +105,19 @@ move_plug = function (new_x, new_y) {
 }
 
 // function to update the x and y of the plug
-update_plug_position = function (newX, newY) {
+function update_plug_position(newX, newY) {
     plug_x = newX - plug_offset_x;
     plug_y = newY - plug_offset_y;
 }
 
 // update the position of the SVG representing the plug with the plug objects x and y
-update_plug_element_position = function () {
+function update_plug_element_position() {
     plug_element.style.left = `${plug_x}px`;
     plug_element.style.top = `${plug_y}px`;
 }
 
 // function to update cord position
-update_cord = function () {
+function update_cord() {
 
     // move the svg element
     cord_svg.style.left = `${play_area_wall_left}px`;
@@ -102,7 +146,7 @@ update_cord = function () {
 }
 
 // update the boundries of the play area
-update_play_area = function () {
+function update_play_area() {
     const socketDiv = document.querySelector(selectors.socketDiv);
     const socketDivOffset = getElementOffset(socketDiv);
     const socketDivAbsoluteWidth = getAbsoluteWidth(socketDiv);
@@ -118,12 +162,12 @@ update_play_area = function () {
     play_area_wall_right = boundingBoxOffset.left + boundingBoxAbsoluteWidth;
 }
 
-update_cord_svg_ceil = function () {
+function update_cord_svg_ceil() {
     cord_svg_ceil = getElementOffset(document.querySelector(selectors.monitor.parent)).top;
 }
 
 // check if the plug is colliding with any of the play area boundries
-check_plug_collision = function () {
+function check_plug_collision() {
     if (is_plugged_in == false) {
 
         // check collision with play area walls
@@ -146,24 +190,24 @@ check_plug_collision = function () {
 }
 
 // function to prevent plug from exceeding its upper Y boundry
-check_hit_floor = function () {
+function check_hit_floor() {
     var boundry = play_area_floor - plug_element.getAttribute("height");
     plug_y = constrain_upper(plug_y, boundry)
 }
 
 // function to prevent plug from exceeding its lower Y boundry
-check_hit_ceil = function () {
+function check_hit_ceil() {
     plug_y = constrain_lower(plug_y, play_area_ceil)
 }
 
 // function to prevent plug from exceeding its lower X boundry
-check_hit_wall_left = function () {
+function check_hit_wall_left() {
     plug_x = constrain_lower(plug_x, play_area_wall_left)
 
 }
 
 // check if you have collided with the socket hitbox
-has_plug_hit_socket = function () {
+function has_plug_hit_socket() {
     var socket_hitbox_y_lower = (socket_y) - socket_hitbox_size;
     var socket_hitbox_y_upper = (socket_y) + socket_hitbox_size;
 
@@ -175,7 +219,7 @@ has_plug_hit_socket = function () {
 }
 
 // checks if the plug is pulled out far enough (if plug_x exceeds the left wall by 1 or more units)
-has_plug_exited_socket = function () {
+function has_plug_exited_socket() {
     if (plug_y == socket_y && plug_x >= play_area_wall_left + 1) {
         return false;
     } else {
@@ -184,7 +228,7 @@ has_plug_exited_socket = function () {
 }
 
 // function to prevent plug from exceeding its lower X socket boundry
-check_hit_end_of_socket = function () {
+function check_hit_end_of_socket() {
     var boundry = play_area_wall_left - depth_of_socket;
 
     plug_x = constrain_lower(plug_x, boundry)
@@ -192,13 +236,13 @@ check_hit_end_of_socket = function () {
 }
 
 // function to prevent plug from exceeding its upper X boundry
-check_hit_wall_right = function () {
+function check_hit_wall_right() {
     var boundry = play_area_wall_right - plug_element.getAttribute("width");
     plug_x = constrain_upper(plug_x, boundry)
 }
 
 // if a value has hit a limit, play the click sound
-play_sound_click_if_hit_boundry = function (value, constraint) {
+function play_sound_click_if_hit_boundry(value, constraint) {
     if (value == constraint && has_sound_click_played == false) {
         sound_click.play();
         has_sound_click_played = true;
@@ -212,7 +256,7 @@ play_sound_click_if_hit_boundry = function (value, constraint) {
 }
 
 // constrains a value to a upper boundry
-constrain_upper = function (value, constraint) {
+function constrain_upper(value, constraint) {
     if (value > constraint) {
         return constraint;
     } else {
@@ -221,7 +265,7 @@ constrain_upper = function (value, constraint) {
 }
 
 // constrains a value to a lower boundry
-constrain_lower = function (value, constraint) {
+function constrain_lower(value, constraint) {
     if (value < constraint) {
         return constraint;
     } else {
@@ -230,7 +274,7 @@ constrain_lower = function (value, constraint) {
 }
 
 // calculate quadratic
-calc_curve = function (cord_start_x, cord_start_y, cord_end_x, cord_end_y, cord_length_max) {
+function calc_curve(cord_start_x, cord_start_y, cord_end_x, cord_end_y, cord_length_max) {
 
     // mid-point of line
     var mpx = (cord_end_x + cord_start_x) * 0.5;
@@ -255,7 +299,7 @@ calc_curve = function (cord_start_x, cord_start_y, cord_end_x, cord_end_y, cord_
 }
 
 // calculates distance of control point from mid-point of line
-calc_offset = function (cord_start_x, cord_end_x, cord_start_y, cord_start_y, cord_length_max) {
+function calc_offset(cord_start_x, cord_end_x, cord_start_y, cord_start_y, cord_length_max) {
     var length_of_x = Math.pow(cord_end_x - cord_start_x, 2);
     var length_of_y = Math.pow(cord_end_y - cord_start_y, 2);
     var length_of_cord = Math.sqrt(length_of_x + length_of_y);
@@ -266,7 +310,7 @@ calc_offset = function (cord_start_x, cord_end_x, cord_start_y, cord_start_y, co
 }
 
 // function for when the screen turns on
-screen_on = function () {
+function screen_on() {
     const monitorScreen = document.querySelector(selectors.monitor.screen);
     const screenText = document.querySelector(selectors.monitor.screenText);
     const powerBtnLight = document.querySelector(selectors.monitor.powerBtnLight);
@@ -278,7 +322,7 @@ screen_on = function () {
 }
 
 // function for when the screen turns off
-screen_off = function () {
+function screen_off() {
     const monitorScreen = document.querySelector(selectors.monitor.screen);
     const screenText = document.querySelector(selectors.monitor.screenText);
     const powerBtnLight = document.querySelector(selectors.monitor.powerBtnLight);
@@ -290,7 +334,7 @@ screen_off = function () {
 }
 
 // function to handle the shifting prongs
-shift_prongs = function () {
+function shift_prongs() {
     var female_large_prong_x = play_area_wall_left;
     var small_prong_female_x = play_area_wall_left - small_prong_female_x_offset;
 
@@ -313,115 +357,36 @@ shift_prongs = function () {
 }
 
 // update socket and its hitboxes x and y values
-update_socket_position = function () {
+function update_socket_position() {
     socket_x = play_area_wall_left - socket_offset_from_wall_left_x;
     socket_y = get_play_area_height_half() + play_area_ceil - socket_offset_from_center_y;
 }
 
 // encapsulating function for updating the play area and its requried parameters
-update_screen_dependent_variables = function () {
+function update_screen_dependent_variables() {
     update_play_area();
     update_cord_svg_ceil();
     update_socket_position();
 }
 
 // instantly moves the plug to the socket
-move_plug_to_socket = function () {
+function move_plug_to_socket() {
     is_plugged_in = true;
     move_plug(socket_x, socket_y);
 }
 
-//getters and setters
-set_is_holding = function (bool) {
-    is_holding = bool;
-}
-
-set_is_hovering = function (bool) {
-    is_hovering = bool;
-}
-
-get_is_holding = function () {
-    return is_holding;
-}
-
-get_is_hovering = function () {
-    return is_hovering;
-}
-
-get_is_plugged_in = function () {
-    return is_plugged_in;
-}
-
-get_play_area_floor = function () {
-    return play_area_floor;
-}
-
-get_play_area_ceil = function () {
-    return play_area_ceil;
-}
-
-get_play_area_center_x = function () {
+function get_play_area_center_x() {
     return (play_area_wall_left + play_area_wall_right) / 2;
 }
 
-get_play_area_height_half = function () {
+function get_play_area_height_half() {
     return (play_area_floor - play_area_ceil) / 2;
 }
 
-get_play_area_height = function () {
+function get_play_area_height() {
     return play_area_floor - play_area_ceil;
 }
 
-get_cord_svg_height = function () {
+function get_cord_svg_height() {
     return play_area_floor - cord_svg_ceil;
 }
-
-// CREATE THE PLUG
-
-// update the boundries of where the plug can exist
-update_screen_dependent_variables();
-move_plug(get_play_area_center_x(), get_play_area_ceil() + get_play_area_height_half());
-
-// when the mouse cursor is over the plug
-plug_element.addEventListener('mouseover', function () {
-    set_is_hovering(true);
-});
-
-// when the mosue cursor is no longer over the plug
-plug_element.addEventListener('mouseout', function () {
-    set_is_hovering(false);
-});
-
-// when the mouse moves
-window.addEventListener('mousemove', (event) => {
-    if (get_is_holding()) {
-        move_plug(event.pageX, event.pageY);
-    }
-});
-
-// when the left mouse button is pressed
-window.addEventListener('mousedown', () => {
-    if (get_is_hovering()) {
-        set_is_holding(true);
-    }
-});
-
-// when the left mouse button is let go
-window.addEventListener('mouseup', () => {
-    if (get_is_holding()) {
-        set_is_holding(false);
-    }
-});
-
-// button for users on mobile devices (the plug cant be moved on mobile/touchscreen devices)
-document.querySelector(selectors.touchscreenBtn).addEventListener('click', (e) => {
-    e.preventDefault();
-    move_plug_to_socket();
-    return false;
-});
-
-// on window resize, update the plug position and the bounding box where the plug can exist
-window.addEventListener('resize', () => {
-    update_screen_dependent_variables();
-    move_plug(plug_x, plug_y);
-});
